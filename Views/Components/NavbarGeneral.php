177485f87c558319c29fe032b627c9d834b094fd
Santiago Becerra <sabecerra06@misena.edu.co>
2021-12-23 22:43:22 -0500

    Forms

--- Views/Components/NavbarGeneral.php
+++ Views/Components/NavbarGeneral.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
+    <div class="container-sm">
         <i class="fas fa-boxes" style="color:#FFFFFF; font-size: larger;" >   
-        <a class="navbar-brand" href="<?= URL ?>Views/Menu/Menu.php"></i>
+        <a class="navbar-brand " href="<?= URL ?>Views/Menu/Menu.php"></i>
             Inventario
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse navbar-text  position-relative" id="navbarNavDropdown">
+        <div class="collapse navbar-collapse navbar-text" id="navbarNavDropdown">
             <ul class="navbar-nav">
-
-                <li class="nav-item dropdown">
+                
+                <li class="nav-item dropdown col-6 col-sm-5">
                     <a class="nav-link dropdown-toggle active" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Empresas
                     </a>
@@ -20,7 +20,7 @@
                     </ul>
                 </li>
 
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown col-6 col-sm-5">
                     <a class="nav-link dropdown-toggle active" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Empleados
                     </a>
@@ -30,7 +30,7 @@
                     </ul>
                 </li>
 
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown col-6 col-sm-5">
                     <a class="nav-link dropdown-toggle active" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Computadores
                     </a>
