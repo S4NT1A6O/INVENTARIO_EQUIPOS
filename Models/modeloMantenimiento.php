a0fa19c71f353031257e896d083039fac2bd8ada
Santiago Becerra <sabecerra06@misena.edu.co>
2022-01-31 12:03:04 -0500

    BUXFIX

---
+++ Models/modeloMantenimiento.php
@@ -0,0 +1,20 @@
+<?php
+
+class modeloMantenimiento{
+
+    // ATRIBUTOS
+    public $ID_MANTENIMIENTO;
+    public $ID_MANTENIMIENTO;
+    public $ID_MANTENIMIENTO;
+    public $ID_MANTENIMIENTO;
+    public $CREATED_AT;
+    public $UPDATED_AT;
+
+    // METODOS
+    // CONSTRUCTOR
+    public function __construct(){
+
+    }
+}
+
+?>
